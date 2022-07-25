a01d4de480c61200add80886673c0e7328bcc02e
Andrew Propst <apropst@gmail.com>
2022-07-25 00:06:33 -0400

    Updated recruit.py

--- solarmark_aauthcogs/cogs/recruit.py
+++ solarmark_aauthcogs/cogs/recruit.py
@@ -63,7 +63,7 @@ class Recruit(commands.Cog):
 	@discord.user_command(name="Recruit for EVE")
 	async def recruit(self, ctx, member: discord.Member):
 		if await self.can_recruit(ctx, member):
-			channel = self.get_channel(settings.SOLARMARK_RECRUIT_CHANID)
+			channel = ctx.get_channel(settings.SOLARMARK_RECRUIT_CHANID)
 			threadname = "RCT-" + member.name
 			messagetext = settings.SOLARMARK_RECRUIT_MSG.format(
 				member.id,
