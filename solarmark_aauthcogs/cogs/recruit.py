989dc671dafb44328aed3da1d2ba5c42ae6618fd
Andrew Propst <apropst@gmail.com>
2022-07-24 21:17:46 -0400

    Updated recruit.py

--- solarmark_aauthcogs/cogs/recruit.py
+++ solarmark_aauthcogs/cogs/recruit.py
@@ -11,12 +11,13 @@ from discord.ext import commands
 # AA Contexts
 from django.conf import settings
 
-
 # Validation Checks - SOLARMARK_THREAD_CHANID and SOLARMARK_THREAD_MSG must be defined in server settings
-if not hasattr(settings, "SOLARMARK_THREAD_CHANID"):
-    raise ValueError("Thread Channel is not defined")
-if not hasattr(settings, "SOLARMARK_THREAD_MSG"):
-    raise ValueError("Thread Message is not defined")
+if not hasattr(settings, "SOLARMARK_RECRUIT_CHANID"):
+    raise ValueError("Recruitment base channel is not defined")
+if not hasattr(settings, "SOLARMARK_RECRUIT_MSG"):
+    raise ValueError("Recruitment message is not defined")
+if not hasattr(settings, "SOLARMARK_RECRUIT_ROLEID"):
+    raise ValueError("Recruitment role ID is not defined")
 
 class Recruit(commands.Cog):
 	"""
@@ -26,14 +27,51 @@ class Recruit(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 
+	async def thread_exist(self, member):
+		for thread in self.get_channel(settings.SOLARMARK_RECRUIT_CHANID).threads:
+			if thread.name == "RCT-" + member.name:
+				return True
+		return False
+
+	async def caller_in_corp(ctx):
+		for userrole in ctx.author.roles:
+			if userrole.id == settings.SOLARMARK_RECRUIT_ROLEID:
+				return True
+		return False
+
+	async def target_not_in_corp(member):
+		for userrole in member.roles:
+			if userrole.id == settings.SOLARMARK_RECRUIT_ROLEID:
+				return False
+		return True
+
+	async def can_recruit(self, ctx, member):
+		if not await self.caller_in_corp(ctx):
+			await ctx.respond("You do not have permission to use this command.", ephemeral = True)
+			return False
+		if not await self.target_not_in_corp(member):
+			await ctx.respond("That user is already in the corporation.", ephemeral = True)
+			return False
+		if await self.thread_exist(self, member):
+			await ctx.respond("That user already has an active recruitment thread.", ephemeral = True)
+			return False
+		return True
+
 	@discord.user_command(name="Recruit for EVE")
 	async def recruit(self, ctx, member: commands.Member):
-		channel = commands.get_channel(settings.SOLARMARK_THREAD_CHANID)
-		threadname = "RCT-" + member.name
-		messagetext = settings.SOLARMARK_THREAD_MSG.format(member.name)
-		await channel.create_thread(name=threadname, message=messagetext, auto_archive_duration=10080, type=private_thread, reason=None)
-	
-	
+		if await self.can_recruit(self, ctx, member):
+			channel = self.get_channel(settings.SOLARMARK_RECRUIT_CHANID)
+			threadname = "RCT-" + member.name
+			messagetext = "\u200b\n\nWelcome <@{}>!\n\nThis is your private thread for moving through the recruitment process with Solarmark. We are happy to have you here and explore having you join the corp!\n\n<@{}> is our CEO.\n<@{}> and <@{}> are our recruitment directors.\n\nWe’re here to answer any questions you have about joining Solarmark.\n\nThe first step in this process will be to set yourself up in AAuth, which is our corporation management tool. This will allow us to take a look at your character(s) and also manages our other services that you'll interact with if you end up joining us.\n\n1. Go to https://aauth.solarmark.io/ and log in with your main character. Provide a valid email as well.\n2. Once logged in, on the dashboard, use the 'Add Character' button to add all your related characters.\n3. Once all characters are added, go to https://aauth.solarmark.io/member-audit/launcher and make sure all your characters are added there as well. If they are not, please manually add them via the 'Register' button.\n4. Still in Member Audit, confirm that all characters have the small white eye icon next to their name. If they do not, click the yellow eye button below their portrait. This gives Solarmark leadership access to view those characters.\n5. Go to https://aauth.solarmark.io/services/ and click the activate button for the Discord service. This will get your Discord account linked to your AAuth account for automated role management.\n\nIf you run into any issues, please message us here and we'll work through them with you.\n\nOnce the AAuth steps above are complete, our process generally is the following:\n1. Q/A back and forth here.\n2. Character(s) background check.\n3. Voice Comms Interview.\n4. Acceptance into corp, assuming everyone is a happy fit.\n\nLooking forward to getting to know each other, feel free to ask any questions and we will take turns responding as available, o7!".format(
+				member.id,
+				97032350247972864,
+				97049182073786368,
+				72091276689801216
+			)
+			threadinfo = await channel.create_thread(name=threadname, auto_archive_duration=10080, type=discord.ChannelType.private_thread)
+			await self.get_channel(threadinfo.id).send(messagetext)
+			await ctx.respond("Recruitment thread created!", ephemeral = True)
+
 def setup(bot):
     """
     Setup the cog
