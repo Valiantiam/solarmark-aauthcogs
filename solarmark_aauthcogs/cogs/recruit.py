d0075dd28e0880312bda2930eb473f935833dda9
apropst <apropst@gmail.com>
2022-07-24 21:22:31 -0400

    Updated recruit.py (#3)
    * Started implementation of permissioning
    
    * Updated recruit.py
    
    * Fixed pulling message from variable.

--- solarmark_aauthcogs/cogs/recruit.py
+++ solarmark_aauthcogs/cogs/recruit.py
@@ -11,12 +11,13 @@ from discord.ext import commands
 # AA Contexts
 from django.conf import settings
 
-
 # Validation Checks - SOLARMARK_THREAD_CHANID and SOLARMARK_THREAD_MSG must be defined in server settings
-if not hasattr(settings, "SOLARMARK_THREAD_CHANID"):
-    raise ValueError("Thread Channel is not defined")
-if not hasattr(settings, "SOLARMARK_THREAD_MSG"):
-    raise ValueError("Thread Message is not defined")
+if not hasattr(settings, "SOLARMARK_RECRUIT_CHANID"):
+    raise ValueError("Recruitment base channel is not defined")
+if not hasattr(settings, "SOLARMARK_RECRUIT_MSG"):
+    raise ValueError("Recruitment message is not defined")
+if not hasattr(settings, "SOLARMARK_RECRUIT_ROLEID"):
+    raise ValueError("Recruitment role ID is not defined")
 
 class Recruit(commands.Cog):
 	"""
@@ -25,16 +26,52 @@ class Recruit(commands.Cog):
 	
 	def __init__(self, bot):
 		self.bot = bot
-		
+
+	async def thread_exist(self, member):
+		for thread in self.get_channel(settings.SOLARMARK_RECRUIT_CHANID).threads:
+			if thread.name == "RCT-" + member.name:
+				return True
+		return False
+
+	async def caller_in_corp(ctx):
+		for userrole in ctx.author.roles:
+			if userrole.id == settings.SOLARMARK_RECRUIT_ROLEID:
+				return True
+		return False
+
+	async def target_not_in_corp(member):
+		for userrole in member.roles:
+			if userrole.id == settings.SOLARMARK_RECRUIT_ROLEID:
+				return False
+		return True
+
+	async def can_recruit(self, ctx, member):
+		if not await self.caller_in_corp(ctx):
+			await ctx.respond("You do not have permission to use this command.", ephemeral = True)
+			return False
+		if not await self.target_not_in_corp(member):
+			await ctx.respond("That user is already in the corporation.", ephemeral = True)
+			return False
+		if await self.thread_exist(self, member):
+			await ctx.respond("That user already has an active recruitment thread.", ephemeral = True)
+			return False
+		return True
 
 	@discord.user_command(name="Recruit for EVE")
 	async def recruit(self, ctx, member: commands.Member):
-		channel = commands.get_channel(settings.SOLARMARK_THREAD_CHANID)
-		threadname = "RCT-" + member.name
-		messagetext = settings.SOLARMARK_THREAD_MSG.format(member.name)
-		await channel.create_thread(name=threadname, message=messagetext, auto_archive_duration=10080, type=private_thread, reason=None)
-	
-	
+		if await self.can_recruit(self, ctx, member):
+			channel = self.get_channel(settings.SOLARMARK_RECRUIT_CHANID)
+			threadname = "RCT-" + member.name
+			messagetext = settings.SOLARMARK_RECRUIT_MSG.format(
+				member.id,
+				97032350247972864,
+				97049182073786368,
+				72091276689801216
+			)
+			threadinfo = await channel.create_thread(name=threadname, auto_archive_duration=10080, type=discord.ChannelType.private_thread)
+			await self.get_channel(threadinfo.id).send(messagetext)
+			await ctx.respond("Recruitment thread created!", ephemeral = True)
+
 def setup(bot):
     """
     Setup the cog
